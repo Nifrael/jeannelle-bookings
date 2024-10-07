ae065f56416994940dd5188ed785ffc132514a30
Julien Marchand <nifrael66@gmail.com>
2024-10-07 21:58:58 +0200

    Gem Devise configurée pour les thérapeutes

--- app/models/therapist.rb
+++ app/models/therapist.rb
@@ -1,4 +1,9 @@
 class Therapist < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
   has_many :availabilities
 
   validates :first_name, :last_name, presence: true
