088eef39bb4e705f1951433cecd601de7cf1311a
Julien Marchand <nifrael66@gmail.com>
2024-10-12 16:10:55 +0200

    Inclusion de Pundit dans le contrôleur

--- app/controllers/therapists_controller.rb
+++ app/controllers/therapists_controller.rb
@@ -2,5 +2,21 @@ class TherapistsController < ApplicationController
 
   def index
     @therapists = Therapist.all
+    authorize @therapists
+  end
+
+  def show
+    @therapist = Therapist.find(params[:id])
+    authorize @therapist
+  end
+
+  def new
+    @therapist = Therapist.new
+    authorize @therapist
+  end
+
+  def create
+    @therapist = Therapist.new(therapist_params)
+    authorize @therapist
   end
 end
