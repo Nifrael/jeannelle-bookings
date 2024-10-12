ab5081f5e4314e0af4a676d56ccbeb706a6bca38
Julien Marchand <nifrael66@gmail.com>
2024-10-12 16:26:48 +0200

    Erreur de syntaxe corrigée

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  include Pundit::Authorization
+
+  def pundit_user
+    current_therapist
+  end
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  def pundit_user
+    current_therapist
+  end
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 end
