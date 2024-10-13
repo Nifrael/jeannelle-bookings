b0ce9d810e14d9b1b6806d237b959eda3e09ea2d
Julien Marchand <nifrael66@gmail.com>
2024-10-13 11:55:25 +0200

    Création des premiers tests pour le modèle

--- test/models/therapist_test.rb
+++ test/models/therapist_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class TherapistTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save therapist without first_name" do
+    therapist = Therapist.new(last_name: "Lol", email: "empty@gmail.com")
+    assert_not therapist.save, "Saved the therapist without a first_name"
+  end
+
+  test "should not save therapist without last_name" do
+    therapist = Therapist.new(first_name: "Lol", email: "empty@gmail.com")
+    assert_not therapist.save, "Saved the therapist without a last_name"
+  end
+
+  test "should not save therapist without email" do
+    therapist = Therapist.new(first_name: "Lol", last_name: "empty")
+    assert_not therapist.save, "Saved the therapist without email"
+  end
+
+    test "should not save therapist without a role" do
+    therapist = Therapist.new(first_name: "Lol", last_name: "empty")
+    assert_not therapist.save, "Saved the therapist without a role"
+  end
 end
