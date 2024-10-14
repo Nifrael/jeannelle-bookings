6dca11b73d4fb88d2a0749962bc05eee1d2f2893
Julien Marchand <nifrael66@gmail.com>
2024-10-14 19:42:23 +0200

    Index des disponibilités

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -1,2 +1,14 @@
 class AvailabilitiesController < ApplicationController
+  before_action :set_therapist
+
+  def index
+    @availabilities = @therapist.availabilities.where(available: true).includes(:therapist)
+  end
+
+  private
+
+  def set_therapist
+    @therapist = Therapist.find(params[:therapist_id])
+  end
+
 end
