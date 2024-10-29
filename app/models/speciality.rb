4c57846b21fb3011f0c04dc81d02722d5f696d77
Julien Marchand <nifrael66@gmail.com>
2024-10-29 23:41:49 +0100

    Modification de la dépendance des tables thérapeutes et spécialités

--- app/models/speciality.rb
+++ app/models/speciality.rb
@@ -1,4 +1,5 @@
 class Speciality < ApplicationRecord
+  has_many :therapist_specialities
   has_many :therapists, through: :therapist_specialities
 
   validates :name, :default_max_attendance, presence: true
