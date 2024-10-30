4c0b0ab7b9fd778285e1f62bbc062075e983d6f6
Julien Marchand <nifrael66@gmail.com>
2024-10-30 20:22:14 +0100

    Début d'écriture des tests

--- test/models/speciality_test.rb
+++ test/models/speciality_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class SpecialityTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save speciality without name" do
+    speciality = Speciality.new(default_max_attendance: 10)
+    assert_not speciality.save, "Saved the speciality without a name"
+  end
 end
