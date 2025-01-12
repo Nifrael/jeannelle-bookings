3fc0fe822566db2ea6b54324e87c15c91e81de52
Julien Marchand <nifrael66@gmail.com>
2025-01-12 22:19:15 +0100

    Séparation des actions CRUD des thérapeutes : new create pour les admins, edit pour les admins et thérapeutes en fonction des paramètres à changer

--- app/controllers/admin/therapists_controllers.rb
+++ app/controllers/admin/therapists_controllers.rb
@@ -1,27 +1,44 @@
 module Admin
   class TherapistsController < ApplicationController
-    before_action :authorize_admin
+    before_action :authorize_admin!
+    before_action :set_therapist, only: [:edit, :update, :destroy]
 
-    def new
-      @therapist = Therapist.new
+  def new
+    @therapist = Therapist.new
+    authorize_therapist
+  end
+
+  def create
+    @therapist = Therapist.new(therapist_params)
+    authorize_therapist
+    if @therapist.save
+      redirect_to @therapist
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
 
-    def create
+  def update
+    @therapist.update(therapist_params)
+    redirect_to therapist_path(@therapist)
+  end
 
-    end
+  def edit
+  end
 
-    def update
-
-    end
-
-    def destroy
-
-    end
+  def destroy
+    @therapist.destroy
+    redirect_to therapists_path
+  end
 
     private
 
-    def set_therapist
+  def set_therapist
+    @therapist = Therapist.find(params[:id])
+  end
 
-    end
+  def authorize_admin!
+    redirect_to root_path, alert: "Accès réservé aux administrateurs." unless current_therapist.admin?
+  end
   end
 end
--- app/controllers/therapists_controller.rb
+++ app/controllers/therapists_controller.rb
@@ -1,5 +1,5 @@
 class TherapistsController < ApplicationController
-  before_action :set_therapist, only: [:show, :edit, :update, :destroy]
+  before_action :set_therapist, only: [:show, :edit, :update]
   before_action :authenticate_therapist!
   before_action :authorize_therapist, only: [:show, :edit, :update, :destroy]
 
@@ -11,32 +11,12 @@ class TherapistsController < ApplicationController
   def show
   end
 
-  def new
-    @therapist = Therapist.new
-    authorize_therapist
-  end
-
-  def create
-    @therapist = Therapist.new(therapist_params)
-    authorize_therapist
-    if @therapist.save
-      redirect_to @therapist
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
-
   def update
     @therapist.update(therapist_params)
     redirect_to therapist_path(@therapist)
   end
 
   def edit
-  end
-
-  def destroy
-    @therapist.destroy
-    redirect_to therapists_path
   end
 
   private
