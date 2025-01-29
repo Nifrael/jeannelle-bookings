9315e6987403ca28dc03753a07818c01952528ee
Julien Marchand <nifrael66@gmail.com>
2025-01-29 23:48:20 +0100

    Ajout de la possibilité de voir tous les thérapeutes via le dashboard d'un thérapeute non admin

--- app/controllers/therapists_dashboard/dashboard_controller.rb
+++ app/controllers/therapists_dashboard/dashboard_controller.rb
@@ -3,6 +3,7 @@ module TherapistsDashboard
     before_action :authenticate_therapist!
 
     def index
+      @therapists = Therapist.all
       @therapist = current_therapist
       @availabilities = current_therapist.availabilities
     end
