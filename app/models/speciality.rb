e106ef4709c6a2c6e848867ebd9d10d653ca3938
Julien Marchand <nifrael66@gmail.com>
2025-01-31 21:59:05 +0100

    dépendance des disponibilités

--- app/models/speciality.rb
+++ app/models/speciality.rb
@@ -1,6 +1,7 @@
 class Speciality < ApplicationRecord
   has_many :therapist_specialities
   has_many :therapists, through: :therapist_specialities
+  has_many :availabilities
 
   validates :name, :default_max_attendance, presence: true
 end
